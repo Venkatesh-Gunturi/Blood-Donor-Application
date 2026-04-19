d1c70e6254b8bba1818c5b1c5e4ca4e634996084
venkatesh <gunturvenkatesh45@gmail.com>
2026-04-19 10:36:11 +0530

    Day 4: Improved registration UI, added Bootstrap design, fixed footer layout, added BOMBAY BLOOD Group support

--- obj/Debug/net8.0/BloodDonorApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/BloodDonorApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodDonorApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7113f3fa937dc214f80e602efc40a638f3d7ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodDonorApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodDonorApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
